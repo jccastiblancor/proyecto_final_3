2d05be428a7dfaa8e7cb2b05b210a0f2d8fc935c
Eloyabmfcv <ea.briceno@uniandes.edu.co>
2020-06-03 15:15:04 -0500

    Traccion listo para probar

--- scripts/traccion.py
+++ scripts/traccion.py
@@ -5,11 +5,13 @@ from std_msgs.msg import String, Int32, Float32MultiArray
 from proyecto_final_3.srv import Navegacion, GridmapPoints
 
 x,y,theta = 0.0
+hayRuta = False
 
 def solicitarRuta():
     """
     main del servicio navegacion
     """
+    global hayRuta
     rospy.init_node('nodo_test')
 
     navegacion = rospy.ServiceProxy('navegacion', Navegacion)
@@ -29,7 +31,7 @@ def solicitarRuta():
 
     print('rutax: "{}"'.format(resp.rutax))
     print('rutay: "{}"'.format(resp.rutay))
-
+    hayRuta = True
 
 def callback_pos(param):
     global x,y,theta
@@ -47,7 +49,7 @@ def acomodar(param):
         velDer = -1
         
     elif param < -0.01:
-		velIzq = -1
+        velIzq = -1
         velDer = 1
 
     elif param >= -0.01 and param <= 0.01:
@@ -58,7 +60,7 @@ def acomodar(param):
 
 #PLS NO TOCAR FALTA INCORPORAR EL CAMBIO DE POSICION FINAL SEGUN LA RUTA Y CAMBIAR DE VELOCIDAD LINEAL Y ANGULAR A LAS VELOCIDADES DE CADA RUEDA
 def traccion_OP():
-	global theta,x,y
+    global theta,x,y,hayRuta
     velocidades = Float32MultiArray()
     R = 0.195
     l = 0.381
@@ -69,25 +71,34 @@ def traccion_OP():
     rospy.Subscriber('/pioneer_position', Twist, callback_pos, queue_size=1)
     pub = rospy.Publisher('/pioneer_motorsVel', Float32MultiArray, queue_size=10)
     rate = rospy.Rate(10)
-
+    ruta = True
     while not rospy.is_shutdown():
-        error = [xf - x,yf-y]
-        rho = np.sqrt(np.power(error[0],2)+np.power(error[1],2))
-        alpha = np.arctan2(error[1],error[0])-theta
-        beta = -thetaf + theta #Falta sacar el thetaf en cada trayecto
+        if hayRuta:
+            for i in length(resp.rutax):
+                xf = resp.rutax[i]
+                yf = resp.rutay[i]
+                thetaf = 0
+                while rho > 0.01 or (beta <= -0.01 and beta >= 0.01):
+                    error = [xf - x,yf-y]
+                    rho = np.sqrt(np.power(error[0],2)+np.power(error[1],2))
+                    alpha = np.arctan2(error[1],error[0])-theta
+                    beta = -thetaf + theta #Falta sacar el thetaf en cada trayecto
 
-        if rho > 0.01:
-            kp = 0.8 + 1.5*np.exp(-rho)
-            v = kp*rho*np.cos(alpha)
-            w = ka*alpha+kp*np.sin(alpha)*np.cos(alpha)
-            velIzq=w*(R - l/2)
-            velDer=w*(R + l/2)
-            velocidades.data = [velIzq, velDer]
-            pub.publish(velocidades)
-            rate.sleep()
-        elif(rho<=0.01):
-            acomodar(beta)
-            rate.sleep()
+                    if rho > 0.01:
+                        kp = 0.8 + 1.5*np.exp(-rho)
+                        v = kp*rho*np.cos(alpha)
+                        w = ka*alpha+kp*np.sin(alpha)*np.cos(alpha)
+                        velIzq=w*(R - l/2)
+                        velDer=w*(R + l/2)
+                        velocidades.data = [velIzq, velDer]
+                        pub.publish(velocidades)
+                        rate.sleep()
+
+                    elif(rho<=0.01):
+                        acomodar(beta)
+                        rate.sleep()
+            hayRuta = False
+        rate.sleep()
 
 if __name__ == '__main__':
     solicitarRuta()
